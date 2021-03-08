e18c6f90a4c96431a833da272a3959a6e2d2ccf7
queuetue (Scott Russell) <scott@queuetue.com>
2021-03-08 18:08:33 -0500

    Create (failing) tests

---
+++ api/src/controllers/ReservationController.spec.ts
@@ -0,0 +1,47 @@
+import { Sequelize } from 'sequelize-typescript'
+import { RouterServer } from '../RouterServer'
+import * as models from '../models'
+import supertest from 'supertest'
+
+describe('ReservationsController ', () => {
+
+  let request: any, sequelize: any, server: any
+
+  beforeAll(async () => {
+    server = new RouterServer()
+    request = supertest(server.app)
+
+    sequelize = new Sequelize(process.env.DATABASE_CONNECTION_STRING, {
+      logging: process.env.LOG === 'debug' ? console.log : false,
+      models: Object.keys(models).map(k => models[k]),
+    })
+
+    await sequelize.sync({
+      force: true
+    })
+
+  });
+
+  it('should not have any reservations', async done => {
+    const resto = await models.Restaurant.create({ name: "Restaurant Zero" })
+    const inventory_item = await models.InventoryItem.create({ name: "Restaurant Zero Inventory Item", restaurantId: resto.id })
+    let url: String, response: any
+    url = `/restaurants/${resto.id}/inventory_items/${inventory_item.id}/reservations`
+    response = await request.get(url)
+    expect(response.status).toBe(200)
+    expect(response.body.count).toBe(0)
+    done()
+  })
+
+  it('should have one reservation', async done => {
+    const resto = await models.Restaurant.create({ name: "Restaurant Zero" })
+    const inventory_item = await models.InventoryItem.create({ name: "Restaurant Zero Inventory Item", restaurantId: resto.id })
+    const reservation = await models.Reservation.create({ inventoryItemId: inventory_item.id, restaurantId: resto.id })
+    let url: String, response: any
+    url = `/restaurants/${resto.id}/inventory_items/${inventory_item.id}/reservations/${reservation.id}`
+    response = await request.get(url)
+    done()
+  })
+
+
+})
